d0f1fa6b62d8220b719efdc01f150815817481d1
DoctorAivan <design.villarroel.s@gmail.com>
2022-07-29 02:24:24 -0400

    first commit

---
+++ src/components/Layout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleCheck } from '@fortawesome/free-solid-svg-icons'
+
+const Layout = () => {
+  return (
+    <div className='bg-slate-200'><FontAwesomeIcon icon={faCircleCheck} /> Layout</div>
+  )
+}
+
+export default Layout
